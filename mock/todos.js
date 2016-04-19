be8cd196361e8afee834188f3e1f20c48d051783
Tom Emmerson <thomasemmerson@sky.com>
2016-04-19 14:14:18 +0100

    updated json to include 2 things

--- mock/todos.js
+++ mock/todos.js
@@ -1,6 +1,6 @@
 [
-	{"name": "task number 1"},
-	{"name": "task number 2"},
-	{"name": "task number 3"},
-	{"name": "task number 4"}
+	{"name": "task name 1", "number": "task number 1"},
+	{"name": "task name 2", "number": "task number 2"},
+	{"name": "task name 3", "number": "task number 3"},
+	{"name": "task name 4", "number": "task number 4"}
 ]
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -4,24 +4,24 @@ angular.module('todoListApp')
 
 .controller('mainCtrl', function($scope, dataService) {
 
-    $scope.learningNgChange = function() {
-        console.log("An input changed.");
-    };
-
+    //when someone presses add new task
     $scope.addTodo = function (todos) {
         var todo = { name: "new task"};
         $scope.todos.push(todo);
     }
 
+    //get todos list upon pageload
     dataService.getTodos(function(response) {
         $scope.todos = response.data;
     });
 
+    //when user presses delete
     $scope.deleteTodo = function (todo, $index) {
         dataService.deleteTodo(todo, $index);
         $scope.todos.splice($index, 1);
     };
 
+    //when user presses save
     $scope.saveTodos = function (todos) {
         var filteredTodos = $scope.todos.filter(function(todo) {
             if (todo.edited) {
@@ -32,4 +32,12 @@ angular.module('todoListApp')
         dataService.saveTodos(filteredTodos);
     };
 
+    $scope.predicate = 'name';
+    $scope.reverse = true;
+
+    //changes sort order
+    $scope.order = function(predicate) {
+        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+        $scope.predicate = predicate;
+    };
 })
